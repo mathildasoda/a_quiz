933b0a4ef546c35841c5a723b7075e0289bedd77
Mathilda Nguyen <mathilda.sodaa@gmail.com>
2020-06-11 00:43:56 -0400

    Initial commit

---
+++ quiz.py
@@ -0,0 +1,15 @@
+#Prompt: 
+#  In this sentence, the number of occurences of 0 is _, of 1 is _, of 2 is _, of 3 is _, of 4 is _, of 5 is _, of 6 is _, of 7 is _, of 8 is _, of 9 is _ 
+#  By: Raphael Robinson
+
+#--- First approach: start from 0 then work up from there --- 
+# Dictionary with 
+
+#-----------------notes--------------------
+#   Creating empty dictionary: Dict={}
+#   Add: Dict[key]=val
+#   Dict[key]=val
+#   del Dict[key]
+#--------------endnotes---------------------
+
+
